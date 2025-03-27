7bf7a6f6f59739e20441c9ce6433b7a1c7ce8750
Juan Camilo Ortiz <juancortizgonz@gmail.com>
2025-03-27 18:37:32 -0500

    Add a global exception handler to show better error messages

---
+++ src/main/java/com/juancortizgonz/patientservice/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.juancortizgonz.patientservice.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> errors.put(error.getObjectName(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
