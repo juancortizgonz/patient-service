e0fa55dea85e82b6f5ced11ed9da35cc3c9eabc3
Juan Camilo Ortiz <juancortizgonz@gmail.com>
2025-03-27 17:41:40 -0500

    Add new service method and define a patient controller

---
+++ src/main/java/com/juancortizgonz/patientservice/controller/PatientController.java
@@ -0,0 +1,26 @@
+package com.juancortizgonz.patientservice.controller;
+
+import com.juancortizgonz.patientservice.dto.PatientResponseDTO;
+import com.juancortizgonz.patientservice.service.PatientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getAllPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+}
